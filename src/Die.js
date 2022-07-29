dfcfcf9e06f9c1b2279c28941fb2f49d53c11377
sft0595 <shafayet.rabby@gmail.com>
2022-07-29 20:59:30 +0600

    Tenzi game

---
+++ src/Die.js
@@ -0,0 +1,18 @@
+import React from "react"
+import { nanoid } from "nanoid"
+
+export default function Die(props) {
+    const styles = {
+        backgroundColor: props.isHeld ? "#59E391" : "white"
+    }
+
+    return (
+        <div 
+            className="die-face" 
+            style={styles}
+            onClick={props.holdDice}
+        >
+            <h2  className="die-num">{Array.from(Array(props.value), (_, i) => <span key={i} className="dot"></span>)}</h2>
+        </div>
+    )
+}
